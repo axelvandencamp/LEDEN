bc13071d8422e0a4cf7b87587d11c81439e5d0f2
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2022-04-20 11:05:18 +0200

    Update LEDEN huidige toestand.sql
    [adres_id] in comment na mislukte wijziging

--- LEDEN huidige toestand.sql
+++ LEDEN huidige toestand.sql
@@ -83,13 +83,13 @@ SELECT	DISTINCT--COUNT(p.id) _aantal, now()::date vandaag
 		ELSE 'andere'
 	END AS provincie,
 	_crm_land(c.id) land,
-	CASE
+	/*CASE
 		WHEN c.id = 21 AND p.crab_used = 'true' THEN
-			c.id || cc.id || ccs.id 
-				|| COALESCE(NULLIF(regexp_replace(p.street_nbr, '\D','','g'),'')::numeric,0) 
-				|| COALESCE(NULLIF(regexp_replace(p.street_bus, '\D','','g'),'')::numeric,0) 
+			c.id::text || cc.id::text || ccs.id::text 
+				|| COALESCE(NULLIF(regexp_replace(p.street_nbr, '\D','','g'),'')::numeric,0)::text 
+				|| COALESCE(NULLIF(regexp_replace(p.street_bus, '\D','','g'),'')::numeric,0)::text
 		ELSE p.id 
-	END adres_id,
+	END adres_id,*/
 	p.email,
 	--COALESCE(ml.id::text,'') ml_id,
 	COALESCE(p.phone_work,p.phone) telefoonnr,
@@ -289,13 +289,13 @@ SELECT	DISTINCT--COUNT(p.id) _aantal, now()::date vandaag
 		ELSE 'andere'
 	END AS provincie,
 	_crm_land(c.id) land,
-	CASE
+	/*CASE
 		WHEN c.id = 21 AND p.crab_used = 'true' THEN
 			c.id + cc.id + ccs.id 
 				+ COALESCE(NULLIF(regexp_replace(p.street_nbr, '\D','','g'),'')::numeric,0) 
 				+ COALESCE(NULLIF(regexp_replace(p.street_bus, '\D','','g'),'')::numeric,0) 
 		ELSE p.id 
-	END adres_id,
+	END adres_id,*/
 	p.email email,
 	--COALESCE(ml.id::text,'') ml_id,
 	COALESCE(p.phone_work,p.phone) telefoonnr,
