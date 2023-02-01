9b64961db041abfef2c3e4c50aca2a4023358885
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2023-02-01 13:42:40 +0100

    Update LEDEN lid via website v2.sql
    aanpassing 2023

--- LEDEN lid via website v2.sql
+++ LEDEN lid via website v2.sql
@@ -3,8 +3,8 @@
 --SET VARIABLES
 DROP TABLE IF EXISTS myvar;
 SELECT 
-	'2022-01-01'::date AS startdatum, 
-	'2022-12-31'::date AS einddatum  --vanaf 01/07 lid tot einde volgende jaar
+	'2023-01-01'::date AS startdatum, 
+	'2023-12-31'::date AS einddatum  --vanaf 01/07 lid tot einde volgende jaar
 INTO TEMP TABLE myvar;
 SELECT * FROM myvar;
 --====================================================================
