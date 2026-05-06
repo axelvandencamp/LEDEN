629825b64ea21c8af3c9a499db71a4832bc33257
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2026-05-06 16:22:54 +0200

    Update LEDEN niet hernieuwden op basis van lidmaatschapslijnen.sql

--- LEDEN niet hernieuwden op basis van lidmaatschapslijnen.sql
+++ LEDEN niet hernieuwden op basis van lidmaatschapslijnen.sql
@@ -86,18 +86,11 @@ INSERT INTO temp_NietHernieuwden (
 		CASE 
 			WHEN c.id = 21 THEN cc.name ELSE p.city 
 		END gemeente,
-		CASE
-			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 1000 AND 1299 THEN 'Brussel' 
-			WHEN p.country_id = 21 AND (substring(p.zip from '[0-9]+')::numeric BETWEEN 1500 AND 1999 OR substring(p.zip from '[0-9]+')::numeric BETWEEN 3000 AND 3499) THEN 'Vlaams Brabant'
-			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 2000 AND 2999  THEN 'Antwerpen' 
-			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 3500 AND 3999  THEN 'Limburg' 
-			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 8000 AND 8999  THEN 'West-Vlaanderen' 
-			WHEN p.country_id = 21 AND substring(p.zip from '[0-9]+')::numeric BETWEEN 9000 AND 9999  THEN 'Oost-Vlaanderen' 
-			WHEN p.country_id = 21 THEN 'Wallonië'
-			WHEN p.country_id = 166 THEN 'Nederland'
-			WHEN NOT(p.country_id IN (21,166)) THEN 'Buitenland niet NL'
-			ELSE 'andere'
-		END AS provincie,
+		CASE WHEN COALESCE(c.id,0) = 21 AND p.zip = '2070' THEN 'Oost-Vlaanderen'
+			WHEN COALESCE(c.id,0) = 21 THEN cs.name
+			WHEN COALESCE(c.id,0) = 166 THEN 'Nederland'
+			ELSE 'Buitenland niet NL' 
+		END provincie_state,
 		_crm_land(c.id) land,
 		COALESCE(COALESCE(p.email,p.email_work),'') email, -- in comment voor belactie
 		/* enkel voor belactie
@@ -183,7 +176,8 @@ INSERT INTO temp_NietHernieuwden (
 
 		
 		--land, straat, gemeente info
-		JOIN res_country c ON p.country_id = c.id
+		LEFT OUTER JOIN res_country_state cs ON cs.id = p.state_id
+		LEFT OUTER JOIN res_country c ON p.country_id = c.id
 		LEFT OUTER JOIN res_country_city_street ccs ON p.street_id = ccs.id
 		LEFT OUTER JOIN res_country_city cc ON p.zip_id = cc.id
 		--herkomst lidmaatschap
